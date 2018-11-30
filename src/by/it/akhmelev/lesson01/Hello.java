c35fbb67186e69c25ec9bdcb8650ae35f184b4b1
Khmelov <375336849110@tut.by>
2018-11-30 21:40:01 +0300

    add readme

--- src/by/it/akhmelev/lesson01/Hello.java
+++ src/by/it/akhmelev/lesson01/Hello.java
@@ -1,4 +1,11 @@
 package by.it.akhmelev.lesson01;
 
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+        // пожалуйста не удаляйте и не меняйте папки
+        // _examples_   - примеры кода
+        // _tasks_      - это задания (копируйте их в свою папку)
+        // akhmelev     - тут будет classwork!
+    }
 }
