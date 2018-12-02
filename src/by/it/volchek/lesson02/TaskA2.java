22d892bca7e2440fe458e175578a2b4c8239b828
volchek-alexandra <volchek.alexandra@gmail.com>
2018-12-02 22:49:33 +0300

    Lesson2

--- src/by/it/volchek/lesson02/TaskA2.java
+++ src/by/it/volchek/lesson02/TaskA2.java
@@ -9,5 +9,11 @@ package by.it.volchek.lesson02;
 */
 
 class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+
+    }
 
 }
