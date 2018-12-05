4e97af9d26a3d4e0ffaf2ea0090345e9e4cb6133
volchek-alexandra <volchek.alexandra@gmail.com>
2018-12-05 22:40:31 +0300

    Lesson2

--- src/by/it/volchek/lesson02/TaskC3.java
+++ src/by/it/volchek/lesson02/TaskC3.java
@@ -1,5 +1,7 @@
 package by.it.volchek.lesson02;
 
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3.86
@@ -30,5 +32,22 @@ package by.it.volchek.lesson02;
 
 */
 class TaskC3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double res = getWeight(i);
+
+        System.out.println(res);
+    }
+   private static double getWeight (int weight) {
+        final double k=3.86;
+        final double j=9.81;
+        double p = weight * k / j;
+        return Math.round(p*100)/100.00;
+    }
+
+
+
 
 }
